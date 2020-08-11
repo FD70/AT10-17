1d69d8edb4e49adb10cc4c2d0ffc2f15596a371b
FD70000000 <halfl.ez.ser@mail.ru>
2020-08-11 23:46:46 +0300

    init

--- src/main/java/day2/Main.java
+++ src/main/java/day2/Main.java
@@ -1,8 +1,15 @@
 package day2;
 
+import java.util.ArrayList;
+
 public class Main {
+    static ArrayList unsorted = new ArrayList();
+
     public static void main(String[] args) {
+        unsorted.add(2);
+        unsorted.add(3);
         System.out.println("hi");
-        day2.MyOwnSort.selectionSort();
+        day2.MyOwnSort.selectionSort(1,2,4);
+        day2.MyOwnSort.selectionSort(unsorted);
     }
 }
--- src/main/java/day2/MyOwnSort.java
+++ src/main/java/day2/MyOwnSort.java
@@ -1,7 +1,17 @@
 package day2;
 
+import java.util.ArrayList;
+
 public class MyOwnSort {
-    public static void selectionSort() {
-        System.out.println("sorded - haha");
+    public static void selectionSort(int ... arguments) {
+        for (int arg: arguments) {
+            System.out.println(arg);
+        }
+    }
+    public static void selectionSort(ArrayList list) {
+        System.out.println("size is" + list.size());
+        for (int i = 0; i < list.size() ;i++) {
+            break;
+        }
     }
 }
