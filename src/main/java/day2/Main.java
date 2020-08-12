aec88bcd22dce821a025f4a10fdc6e42af297395
FD70000000 <halfl.ez.ser@mail.ru>
2020-08-12 20:02:28 +0300

    day2 add MyOwnSort

--- src/main/java/day2/Main.java
+++ src/main/java/day2/Main.java
@@ -3,13 +3,18 @@ package day2;
 import java.util.ArrayList;
 
 public class Main {
+    static int COUNT_OF_NUMBERS = 10;
+    static int MAX_NUMBER = 42;
     static ArrayList<Integer> unsorted = new ArrayList<>();
 
+    private static void fillArrayWithRandom(ArrayList<Integer> list, int count) {
+        for (int i = 0; i < count; i++) {
+            list.add((int) (Math.random() * MAX_NUMBER + 1));
+        }
+    }
+
     public static void main(String[] args) {
-        unsorted.add(2);
-        unsorted.add(3);
-        System.out.println("hi");
-        MyOwnSort.selectionSort(1,2,4);
+        fillArrayWithRandom(unsorted, COUNT_OF_NUMBERS);
         MyOwnSort.selectionSort(unsorted);
     }
 }
--- src/main/java/day2/MyOwnSort.java
+++ src/main/java/day2/MyOwnSort.java
@@ -3,15 +3,33 @@ package day2;
 import java.util.ArrayList;
 
 public class MyOwnSort {
-    public static void selectionSort(int ... arguments) {
-        for (int arg: arguments) {
-            System.out.println(arg);
+
+    private static void printArray (ArrayList<Integer> list) {
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
         }
+        System.out.println();
     }
-    public static void selectionSort(ArrayList list) {
-        System.out.println("size is" + list.size());
-        for (int i = 0; i < list.size() ;i++) {
-            break;
+
+    private static void replaceTwo(ArrayList<Integer> list, int index1, int index2) {
+        int temp = list.get(index1);
+        list.set(index1, list.get(index2));
+        list.set(index2, temp);
+    }
+
+    public static void selectionSort(ArrayList<Integer> list) {
+        System.out.println("Before sort");
+        printArray(list);
+
+        for (int min = 0; min < list.size() ;min++) {
+            for (int i = min + 1; i < list.size(); i++) {
+                if (list.get(i) <  list.get(min)) {
+                    replaceTwo(list, i, min);
+                }
+            }
         }
+
+        System.out.println("After sort");
+        printArray(list);
     }
 }
