e5e2ebcc37201ef2c56c1e86ab609baed5116edd
FD70000000 <halfl.ez.ser@mail.ru>
2020-08-17 16:55:05 +0300

    init README.md f day3

--- src/test/java/day3/SelenidPages/openBank/MainPage.java
+++ src/test/java/day3/SelenidPages/openBank/MainPage.java
@@ -1,5 +1,6 @@
 package day3.SelenidPages.openBank;
 
 public class MainPage {
-    //tr[2]//td[2]//*[@class="main-page-exchange__rate"]
+
+
 }
--- src/test/java/day3/myOwnTest/WithSelenide.java
+++ src/test/java/day3/myOwnTest/WithSelenide.java
@@ -1,4 +1,14 @@
 package day3.myOwnTest;
 
+import org.testng.annotations.Test;
+
 public class WithSelenide {
+
+    @Test
+    public void withSelenide() {
+        // with page object
+        // "https://www.open.ru";
+        // извлечь курсы валют и проверить, что курс продажи доллара выше, чем курс покупки.
+        // Аналогичная проверка - для евро.
+    }
 }
