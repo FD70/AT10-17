fcb0cad97f995d0fb6727490b23b18d5ca5f8786
FD70000000 <halfl.ez.ser@mail.ru>
2020-08-17 19:30:56 +0300

    release WithClearSelenium

--- src/test/java/day3/myOwnTest/WithClearSelenium.java
+++ src/test/java/day3/myOwnTest/WithClearSelenium.java
@@ -1,7 +1,9 @@
 package day3.myOwnTest;
 
 import day3.CDF.ChromeDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class WithClearSelenium {
@@ -19,15 +21,29 @@ public class WithClearSelenium {
 
         driver.get(baseURL);
         // Мы на главной странице
+        //Извлечь курсы валют
+        Double usdBuy = getCourse("//tr[2]//td[2]//*[@class=\"main-page-exchange__rate\"]");
+        Double usdTrade = getCourse("//tr[2]//td[4]//*[@class=\"main-page-exchange__rate\"]");
+        Double eurBuy = getCourse("//tr[3]//td[2]//*[@class=\"main-page-exchange__rate\"]");
+        Double eurTrade = getCourse("//tr[3]//td[4]//*[@class=\"main-page-exchange__rate\"]");
 
-        //tr[2]//td[2]//*[@class="main-page-exchange__rate"]
+        System.out.println("usdBuy " + usdBuy);
+        System.out.println("usdTrade " + usdTrade);
+        System.out.println("eurBuy " + eurBuy);
+        System.out.println("eurTrade " + eurTrade);
 
-        //Извлечь курсы валют и проверить, что курс продажи доллара выше, чем курс покупки.
+        // ..и проверить, что курс продажи доллара выше, чем курс покупки.
         //Аналогичная проверка - для евро.
+        Assert.assertTrue(usdBuy < usdTrade);
+        Assert.assertTrue(eurBuy < eurTrade);
     }
 
     @AfterClass
     public void closeBrowser() {
         driver.quit();
+    }
+
+    public Double getCourse(String xPath) {
+        return Double.parseDouble(driver.findElement(By.xpath(xPath)).getText().replace(",","."));
     }
 }
