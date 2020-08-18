d94ed6ecad86ea134e9e17ef2b40ebc2a4c7fd56
FD70000000 <halfl.ez.ser@mail.ru>
2020-08-18 12:51:31 +0300

    dayX VtbCalcTest release v1

--- src/test/java/dayX/SelenidPages/VTB/VtbExchangePage.java
+++ src/test/java/dayX/SelenidPages/VTB/VtbExchangePage.java
@@ -1,5 +1,77 @@
 package dayX.SelenidPages.VTB;
 
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.Wait;
+import static com.codeborne.selenide.Selenide.open;
+
 public class VtbExchangePage {
-    // init
+    private static String  url = "https://www.vtb.ru/personal/platezhi-i-perevody/obmen-valjuty/";
+    static String USD = "USD";
+    static String RUR = "RUR";
+    static String EUR = "EUR";
+
+
+
+    private void method1change(String val) {
+        $x("//input[@id=\"method1\"]/../../div[3]").click();
+        $x("//input[@id=\"method1\"]/../..//li[contains(.,\"" + val + "\")]").click();
+    }
+    private void method2change(String val) {
+        $x("//input[@id=\"method2\"]/../../div[3]").click();
+        $x("//input[@id=\"method2\"]/../..//li[contains(.,\"" + val + "\")]").click();
+    }
+    private void exchangeMethod (String have, String want) {
+        method1change(have);
+        method2change(want);
+    }
+
+    public static VtbExchangePage getExchangePage() {
+        return open(url, VtbExchangePage.class);
+    }
+
+    // rur eur usd
+    public Double getRurToUsdCourse (Double rub) {
+        return getCourse(rub, RUR, USD);
+    }
+    public Double getRurToEurCourse (Double rub) {
+        return getCourse(rub, RUR, EUR);
+    }
+    public Double getUsdToRurCourse (Double usd) {
+        return getCourse(usd, USD, RUR);
+    }
+    public Double getUsdToEurCourse (Double usd) {
+        return getCourse(usd, USD, EUR);
+    }
+    public Double getEurToRurCourse (Double eur) {
+        return getCourse(eur, EUR, RUR);
+    }
+    public Double getEurToUsdCourse (Double eur) {
+        return getCourse(eur, EUR, USD);
+    }
+    private Double getCourse (Double val, String m1, String m2) {
+        exchangeMethod(m1, m2);
+
+        $x("//input[@id=\"method1\"]").clear();
+        String oldM2value = $x("//input[@id=\"method2\"]").val();
+        $x("//input[@id=\"method1\"]").sendKeys(val.toString().replace(".",","));
+        //клик на название, чтобы метод вернул курс
+        $x("//input[@id=\"method1\"]/../../../label").click();
+
+        // проверка, и ожидание, что курс рассчитался
+        String result  = oldM2value;
+        for (int i = 0; i < 10; i++) {
+            result = $x("//input[@id=\"method2\"]").val();
+            if (oldM2value.equals(result) || result.equals("0")) {
+                try {
+                    Thread.sleep(500);
+                } catch (Exception ignored) {}
+            } else {
+                break;
+            }
+        }
+        //FIXME почему-то replace отказывается работать в этом месте
+        result = result.replaceAll("\\s", "").replace(",",".");
+        System.out.println(result);
+        return Double.parseDouble(result);
+    }
 }
--- src/test/java/dayX/VtbCalcTest.java
+++ src/test/java/dayX/VtbCalcTest.java
@@ -1,8 +1,43 @@
 package dayX;
 
+import com.codeborne.selenide.Configuration;
+import dayX.SelenidPages.VTB.VtbExchangePage;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+
 public class VtbCalcTest {
-    // https://www.vtb.ru/personal/platezhi-i-perevody/obmen-valjuty/
     // Используйте только Selenide.
     // Написать минимум четыре тест кейса
     // - рубли в доллары, доллары в рубли, до $30000, более $30000
+
+    private VtbExchangePage page;
+    @BeforeClass
+    public void before() {
+        page = VtbExchangePage.getExchangePage();
+        $x("//*[@id=\"cookiemsgoffpls\"]/p[1]").click();
+    }
+    @Test
+    public void test1() {
+        System.out.println(page.getRurToUsdCourse(100.0));
+    }
+
+    @Test
+    public void test3() {
+        System.out.println(page.getRurToEurCourse(29000.0));
+    }
+
+    @Test
+    public void test4() {
+        System.out.println(page.getEurToRurCourse(31000.0));
+    }
+
+    @Test
+    public void test2() {
+        System.out.println(page.getUsdToRurCourse(100.0));
+    }
 }
